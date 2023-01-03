23f21df6de1f147e7ebebe75427682749038ff7d
Kaio Mota <kaiomotalu@gmail.com>
2023-01-03 14:46:13 -0300

    Atualização do servidor usando Express

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const app = require('express')
+const express = require('express')
+
+const app = express()
+const porta = 3000
+
+app.get('/json', (req, res) => {
+  res.json({ title: 'tarefa exemplo', done: true })
+})
+
+app.get('/', (req, res) => {
+  res.send('<h1>Lista de tarefas :D</h1>')
+})
+
+app.listen(porta, () => {
+  console.log(`App example funcionando na porta ${porta}`)
+})
